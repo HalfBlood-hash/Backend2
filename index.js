3fec8813d6978bc9d34243c1f97fa6ff6cc6e917
HalfBlood-hash <sanusatyam28@gmail.com>
2025-08-10 09:12:04 +0530

    first commint

---
+++ index.js
@@ -0,0 +1,15 @@
+
+console.log("starat")
+const express =require("express")
+const app=express();
+require("dotenv").config();
+const port=process.env.PORT|| 4000;
+
+app.get('/',(req,res)=>{
+    res.send("helloworld");
+})
+
+
+app.listen(port,()=>{
+    console.log("listening on port ",port);
+})
