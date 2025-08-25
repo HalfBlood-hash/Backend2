152ba8fd1775fd26d685b91f1e51ffa2a06b4d79
HalfBlood-hash <sanusatyam28@gmail.com>
2025-08-25 09:40:54 +0530

    util file model jst token bycryt hashing apierror class added

--- Backend3-full/src/index.js
+++ Backend3-full/src/index.js
@@ -1,31 +1,21 @@
 
 
 import dotenv from "dotenv";
-import express from "express";
+import {app} from "./app.js";
 import connectDB from "./db/db.js";
 
 dotenv.config({
     path: './.env'
 });
+const PORT =process.env.PORT|| 8001
+connectDB()
+.then(()=>{
+    app.listen(PORT,()=>{
+        console.log("app is running on port :",PORT)
+    })
+})
+.catch((error) => {
+    console.log("connection failed !!!",error);
+});
 
-connectDB();
-// const app = express();
-
-// ;(async () => {
-//     try {
-//         await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`);
-//         console.log(`\n MongoDB connected !! DB HOST: ${mongoose.connection.host}`);
-
-//         app.on("error", (error) => {
-//             console.error("EXPRESS ERROR: ", error);
-//         })
-
-//         app.listen(process.env.PORT || 8000, () => {
-//             console.log(`Server is running at port : ${process.env.PORT || 8000}`);
-//         })
-//     } catch (error) {
-//         console.error("MONGODB connection FAILED: ", error);
-//         process.exit(1);
-//     }
-// })()
 
---
+++ Backend3-full/src/model/video.modal.js
@@ -0,0 +1,44 @@
+import { Schema } from "mongoose";
+import mongooseAggregatePaginatev2 from "mongoose-aggregate-paginate-v2";
+
+import mongoose,{Schema,model} from "mongoose"
+
+const videoSchema =new Schema({
+    videoFile: {
+        type: String, //cloudinary url
+        required: true
+    },
+    thumbnail: {
+        type: String, //cloudinary url
+        required: true
+    },
+    title: {
+        type: String, 
+        required: true
+    },
+    description: {
+        type: String, 
+        required: true
+    },
+    duration: {
+        type: Number, 
+        required: true
+    },
+    views: {
+        type: Number,
+        default: 0
+    },
+    isPublished: {
+        type: Boolean,
+        default: true
+    },
+    owner: {
+        type: Schema.Types.ObjectId,
+        ref: "User"
+    }
+},{timestamps:true})
+
+
+videoSchema.plugin(mongooseAggregatePaginatev2)
+
+export const Video=mongoose.model('Video',videoSchema)
