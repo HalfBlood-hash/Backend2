c90f059e0a297b61ea719d6e0c229803130fb72f
HalfBlood-hash <sanusatyam28@gmail.com>
2025-10-17 01:40:50 +0530

    updation function of avatar coverimage fullname email change password function and get the current user detials

--- Backend3-full/src/controllers/user.controllers.js
+++ Backend3-full/src/controllers/user.controllers.js
@@ -6,6 +6,7 @@ import {uploadOnCloudinary} from '../utils/cloudinary.js'
 import {ApiResponse} from '../utils/ApiRespone.js'
 import jwt from "jsonwebtoken"
 
+
 const generateAccessTokenAndRefreshToken=async(userId)=>{
     try {
         
@@ -171,4 +172,94 @@ const refreshAccessToken=asyncHandler(async(req,res)=>{
                 {accessToken,refreshToken},"Access token refreshed sucessfully")
         )
 })
-export {registerUser,loginUser,logoutUser,refreshAccessToken}
+
+const changePassword=asyncHandler(async(req,res)=>{
+    // get old and new password from req body
+    const {oldPassword,newPassword}=req.body;
+    // check if the old password or new password is empty
+    if([oldPassword,newPassword].some((feild)=>feild?.trim()==="")) {   
+            throw new ApiError(400," All Feilds are required ");}
+    // get the user from db
+    const user =await User.findById(req.user._id);
+   const isPasswordCorrect=await user.isPasswordCorrect(oldPassword)
+   if(!isPasswordCorrect) throw new ApiError(401," Old Password is incorrect")
+    
+    // save the password in db
+    user.password=newPassword;
+    await user.save();
+
+    return res.status(200).json(
+        new ApiResponse(200,{}," Password changed sucessfully")
+    )
+})
+
+const getUserProfile=asyncHandler(async(req,res)=>{
+    const user=await User.findById(req.user._id).select(
+        "-password -refreshToken -watchHistory"
+    )  
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200,user,"User profile fetched sucessfully")
+    )
+})
+
+const updateUserdetails=asyncHandler(async(req,res)=>{
+    // get the detail to be updated from req body
+    const {fullName,email}=req.body;
+    // check if the fullName or email is empty
+    if([fullName,email].some((feild)=>feild?.trim()==="")) {    
+            throw new ApiError(400," All Feilds are required ");}
+    // check if the email is already in use by another user
+    const existingUser=await User.findOne({email:email})
+    // if the existing user is present and the existing user id is not same as the logged in user id
+    if(existingUser && existingUser._id.toString()!==req.user._id.toString())
+    {
+        throw new ApiError(409," email is already in use by another user")
+    }
+    // update the user details in db
+    const updatedUser=await User.findByIdAndUpdate(
+        req.user._id,
+        {fullName,email:email},
+        {new:true}).select("-password -refreshToken -watchHistory")
+
+        return res.status(200).json(
+            new ApiResponse(200,updatedUser," User details updated sucessfully")
+        )
+    
+})
+const updateAvatar=asyncHandler(async(req,res)=>{
+    // get the avatar from req file
+    const avatarLocalPath=req.file?.path;
+    if(!avatarLocalPath) throw new ApiError(400," Avatar is required ");
+    // upload the avatar to cloudinary
+    const avatar= await uploadOnCloudinary(avatarLocalPath);
+    if(! avatar) throw new ApiError(400," Avatari is missing during upload");
+    // update the avatar in db
+    const updatedUser=await User.findByIdAndUpdate(
+        req.user._id,
+        {avatar:avatar.url},
+        {new:true}).select("-password -refreshToken -watchHistory")
+
+    return res.status(200).json(
+        new ApiResponse(200,updatedUser," Avatar updated sucessfully")
+    )
+})
+const updateCoverImage=asyncHandler(async(req,res)=>{
+    // get the cover image from req file
+    const coverImageLocalPath=req.file?.path;
+    if(!coverImageLocalPath) throw new ApiError(400," Cover Image is required ");
+    // upload the cover image to cloudinary
+    const coverImage= await uploadOnCloudinary(coverImageLocalPath);
+    if(! coverImage) throw new ApiError(400," Cover Image is required missing during upload");
+    // update the cover image in db
+    const updatedUser=await User.findByIdAndUpdate(
+        req.user._id,
+        {coverImage:coverImage.url},
+        {new:true}).select("-password -refreshToken -watchHistory")
+
+    return res.status(200).json(
+        new ApiResponse(200,updatedUser," Cover Image updated sucessfully")
+    )
+})
+export {registerUser,loginUser,logoutUser,refreshAccessToken,changePassword,getUserProfile,updatedUser,updateAvatar}
